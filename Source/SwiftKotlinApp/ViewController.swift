a97f5ed0cff7d75b747ad7ab92191398725920fd
Angel Garcia <angelgarcia.mail@gmail.com>
2016-11-09 19:50:41 +0100

    Added attributes to text

--- Source/SwiftKotlinApp/ViewController.swift
+++ Source/SwiftKotlinApp/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
@@ -42,16 +41,66 @@ class ViewController: NSViewController {
         })
     }
     
-    func translateSwift() {
+    func translateSwift(withSwiftFormatting: Bool = true, withKotlinFormatting: Bool = true) {
+        
         let swift = swiftTextView.string ?? ""
-        let kotlin = try? swiftKotlin.translate(content: swift)
-        kotlinTextView.string = kotlin
+        let swiftTokens = tokenize(swift)
+        let kotlinTokens = (try? self.swiftKotlin.translate(tokens: swiftTokens)) ?? []
+        
+        DispatchQueue.main.async {
+            self.swiftTextView.textStorage?.beginEditing()
+            self.kotlinTextView.textStorage?.beginEditing()
+            
+            if withSwiftFormatting {
+                let formatted = self.attributedStringFromTokens(tokens: swiftTokens)
+                self.swiftTextView.textStorage?.setAttributedString(formatted)
+            }
+
+            if withKotlinFormatting {
+                let formatted = self.attributedStringFromTokens(tokens: kotlinTokens)
+                self.kotlinTextView.textStorage?.setAttributedString(formatted)                
+            }
+            else {
+                self.kotlinTextView.string = kotlinTokens.reduce("", { $0! + $1.string })
+            }
+            
+            self.swiftTextView.textStorage?.endEditing()
+            self.kotlinTextView.textStorage?.endEditing()
+        }
+    }
+    
+    func attributedStringFromTokens(tokens: [Token]) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString()
+        tokens.forEach {
+            attributedString.append($0.attributedString)
+        }
+        return attributedString
     }
 }
 
 extension ViewController: NSTextViewDelegate {
     func textDidChange(_ notification: Notification) {
         translateSwift()
+    }
+}
+
+
+extension Token {
+    var attributes: [String : Any] {
+        switch self {
+        case .keyword(_):
+            return [NSForegroundColorAttributeName: NSColor(red: 170.0/255.0, green: 13.0/255.0, blue: 145.0/255.0, alpha: 1)]
+        case .commentBody(_):
+            return [NSForegroundColorAttributeName: NSColor(red: 0, green: 116.0/255.0, blue: 0, alpha: 1)]
+        case .stringBody(_), .startOfScope("\""), .endOfScope("\""):
+            return [NSForegroundColorAttributeName: NSColor(red: 196.0/255.0, green: 26.0/255.0, blue: 22.0/255.0, alpha: 1)]
+        default:
+            return [:]
+        }
+    }
+    
+    var attributedString: NSAttributedString {
+        return NSAttributedString(string: self.string, attributes: self.attributes)
     }
 }
 
