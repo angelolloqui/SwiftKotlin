b2fd3cc3f4b2baab9fb520a296dd79dfce907d06
Jon Nermut <jon.nermut@asdeqlabs.com>
2017-05-03 20:27:33 +1000

    Add an integration test script that tests end to end transpiling and running

---
+++ IntegrationTest/constructors.swift
@@ -0,0 +1,30 @@
+
+
+open class BaseError
+{
+	let message: String
+	let cause: String
+
+	public init(message: String, cause: String)
+	{
+		self.message = message
+		self.cause = cause
+	}
+}
+
+open class SomeError : BaseError
+{
+
+
+	public convenience init()
+	{
+		self.init(message: "hello")
+	}
+
+	public init(message: String)
+	{
+		super.init(message: message, cause: "")
+	}
+
+}
+
---
+++ IntegrationTest/hello.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("Hello, World!")
+
+    val e = SomeError()
+    println("ok: constructors ${e.message}")
+
+
+}
+
