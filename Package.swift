6bb4b8edeae63e98a3ae2a8b863ecb3af3cc649f
Angel Luis Garcia <angelgarcia.mail@gmail.com>
2017-08-26 16:23:58 +0200

    Removed submodules and used SPM

---
+++ Package.swift
@@ -0,0 +1,63 @@
+// swift-tools-version:4.0
+
+
+import PackageDescription
+
+let package = Package(
+  name: "SwiftKotlin",
+  products: [
+    .executable(
+      name: "SwiftKotlinCommandLine",
+      targets: [
+        "SwiftKotlinCommandLine",
+      ]
+    ),
+    .executable(
+        name: "SwiftKotlinApp",
+        targets: [
+            "SwiftKotlinApp",
+        ]
+    ),
+    .library(
+      name: "SwiftKotlinFramework",
+      targets: [
+        "SwiftKotlinFramework",
+      ]
+    ),
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/yanagiba/swift-transform",
+      .revision("bcd99a671f5376ef7d3c7f8d45a0297a94678785")
+    ),
+  ],
+  targets: [
+    .target(
+      name: "SwiftKotlinFramework",
+      dependencies: [
+        "swift-transform",
+      ]
+    ),
+    .target(
+        name: "SwiftKotlinApp",
+        dependencies: [
+            "SwiftKotlinFramework",
+        ]
+    ),
+    .target(
+        name: "SwiftKotlinCommandLine",
+        dependencies: [
+            "SwiftKotlinFramework",
+        ]
+    ),
+
+    // MARK: Tests
+    .testTarget(
+      name: "SwiftKotlinFrameworkTests",
+      dependencies: [
+        "SwiftKotlinFramework",
+      ]
+    ),    
+  ],
+  swiftLanguageVersions: [4]
+)
