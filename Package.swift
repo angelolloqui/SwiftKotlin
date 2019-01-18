916560c25a6ad42b482910e0c4e439a2d8d6385b
Angel Luis Garcia <angelgarcia.mail@gmail.com>
2019-01-18 20:30:10 +0100

    Updated travis file and cleanup

--- Package.swift
+++ Package.swift
@@ -4,39 +4,34 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftKotlinFramework",
-  products: [
-    .library(
-      name: "SwiftKotlinFramework",
-      targets: [
-        "SwiftKotlinFramework",
-      ]
-    ),
-  ],
-  dependencies: [
-    .package(
-      url: "https://github.com/yanagiba/swift-transform",
-      .exact("0.18.10")
-    )    
-  ],
-  targets: [
-    .target(
-      name: "SwiftKotlinFramework",
-      dependencies: [
-        "swift-transform",
-      ],
-      exclude: [
-        "SwiftKotlin.xcworkspace"
-      ]
-    ),    
+    name: "SwiftKotlinFramework",
+    products: [
+        .library(
+            name: "SwiftKotlinFramework",
+            targets: ["SwiftKotlinFramework"]
+        )],
+    dependencies: [
+        .package(url: "https://github.com/yanagiba/swift-transform", .exact("0.18.10"))
+    ],
+    targets: [
 
-    // MARK: Tests
-    .testTarget(
-      name: "SwiftKotlinFrameworkTests",
-      dependencies: [
-        "SwiftKotlinFramework",
-      ]
-    ),    
-  ],
-  swiftLanguageVersions: [4]
+        // MARK: Main framework
+        .target(
+            name: "SwiftKotlinFramework",
+            dependencies: ["swift-transform"],
+            exclude: [
+                "SwiftKotlin.xcworkspace"
+            ]
+        ),
+
+        // MARK: Tests
+        .testTarget(
+            name: "SwiftKotlinFrameworkTests",
+            dependencies: ["SwiftKotlinFramework"],
+            exclude: [
+                "SwiftKotlin.xcworkspace"
+            ]
+        )
+    ],
+    swiftLanguageVersions: [4]
 )
